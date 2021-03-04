cd89d67d4916bf2cd7a84ae8986cdb29a0425741
NGUYEN Le Ha Vy <le-ha-vy.a.nguyen@inria.fr>
2021-03-04 16:08:16 +0100

    add downloading labeled dataset

--- api/src/config/general.py
+++ api/src/config/general.py
@@ -1,7 +1,9 @@
 import os
 
 UPLOAD_FOLDER = os.path.join(__file__.split(sep="api")[0], "api", "sessions")
-FILE_NAME = "data.csv"
+DATASET_FILE = "data.csv"
+LABELED_DATASET_FILE = "labeled_dataset.csv"
+
 
 SESSION_EXPIRY_TIME_IN_SECONDS = 2 * 24 * 60 * 60
 
--- api/src/utils.py
+++ api/src/utils.py
@@ -1,6 +1,6 @@
 import os
 
-from .config.general import UPLOAD_FOLDER, FILE_NAME
+from .config.general import UPLOAD_FOLDER, DATASET_FILE, LABELED_DATASET_FILE
 from .db import db_client
 
 SESSION_EXPIRED_MESSAGE = {"errorMessage": "Session expired"}
@@ -11,7 +11,11 @@ def get_session_path(session_id):
 
 
 def get_dataset_path(session_id):
-    return os.path.join(get_session_path(session_id), FILE_NAME)
+    return os.path.join(get_session_path(session_id), DATASET_FILE)
+
+
+def get_labeled_dataset_path(session_id):
+    return os.path.join(get_session_path(session_id), LABELED_DATASET_FILE)
 
 
 def is_session_expired(session):
